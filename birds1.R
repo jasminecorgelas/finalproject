94464aabaac9c3f7c7defd7177909c5284ea0d9d
jasminecorgelas <122766814+jasminecorgelas@users.noreply.github.com>
2023-05-01 16:50:44 -0400

    Add files via upload

---
+++ birds1.R
@@ -0,0 +1,9 @@
+# @title Birds
+# @description this function provides the average number of birds across all the safari's
+# @param x is the input values of the data you find
+# @ return resulting constant for main function
+
+birds <- function(x){
+  my_birds <- (mean(x))
+  return(my_birds)
+}
